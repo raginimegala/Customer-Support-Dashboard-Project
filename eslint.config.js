fd3c9bf22dbd86e4eba142d0c0fd3253aea50678
raginimegala <ragini.megala@gmail.com>
2026-06-03 14:50:05 +0530

    test husky

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,58 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+    ignores: ['node_modules', 'dist', 'coverage'],
+
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+
     languageOptions: {
+      parser: tsParser,
+
       globals: globals.browser,
+
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
     },
+
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      '@typescript-eslint': tseslint,
+      prettier: prettierPlugin
+    },
+
+    rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+
+      'prettier/prettier': 'error',
+
+      'react/react-in-jsx-scope': 'off',
+
+      '@typescript-eslint/no-unused-vars': 'warn'
+    },
+
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    }
   },
-])
+
+  prettierConfig
+];
